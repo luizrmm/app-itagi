d63f65e0ebf06eb41a6f76ab5fbf648c7f43d902
luizrmm <luizrmm97@gmail.com>
2019-12-14 12:54:44 -0300

    telas de enquete terminada

--- lib/screens/Enquete/enquetes_part.dart
+++ lib/screens/Enquete/enquetes_part.dart
@@ -1,4 +1,7 @@
+import 'package:aqui_cliente/notifiers/enquetes/enquetes_usuario.dart';
+import 'package:aqui_cliente/screens/widgets/loading.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'widgets/tile_enquete.dart';
 
@@ -9,24 +12,49 @@ class EnquetesPart extends StatefulWidget {
 
 class _EnquetesPartState extends State<EnquetesPart> {
   @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<UsuarioEnqueteNotifier>(context).getEnquetesUsuario());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.fromLTRB(8.0, 12.0, 8.0, 0.0),
-        child: ListView(
-          children: <Widget>[
-            TileEnquete(
-              iconData: Icons.check,
+    if (Provider.of<UsuarioEnqueteNotifier>(context).loading) {
+      return Container(
+        child: Center(
+          child: Loading(),
+        ),
+      );
+    } else if (Provider.of<UsuarioEnqueteNotifier>(context).usuarioEnquetes ==
+        null) {
+      return Container(
+        child: Center(
+          child: Text('Nenhuma enquete foi encontrada'),
+        ),
+      );
+    }
+    return Consumer<UsuarioEnqueteNotifier>(
+      builder: (context, result, child) {
+        int qtd = result.usuarioEnquetes.length;
+        return ListView.builder(
+          itemCount: qtd,
+          itemBuilder: (BuildContext context, int index) {
+            return TileEnquete(
+              id: result.usuarioEnquetes[index].id,
+              title: result.usuarioEnquetes[index].titulo,
+              description: result.usuarioEnquetes[index].enquete,
+              qtdVotos: result.usuarioEnquetes[index].qtdVotos,
+              aFavor: result.usuarioEnquetes[index].vFavor,
+              contra: result.usuarioEnquetes[index].vContra,
+              dataEnc: result.usuarioEnquetes[index].dataFinal,
+              encerrada: true,
               color: Colors.green,
-            ),
-            TileEnquete(
-              iconData: Icons.check,
-              color: Colors.green,
-            ),
-            TileEnquete(
-              iconData: Icons.check,
-              color: Colors.green,
-            ),
-          ],
-        ));
+              iconData: Icons.check_circle,
+            );
+          },
+        );
+      },
+    );
   }
 }
